0e795ccabd329e4d53950e6f5d25adab857b93fa
Ilay_(Sleepy Mita) <148774440+Ilay4133@users.noreply.github.com>
2025-03-07 21:07:52 +0700

    Update home_page_add_new_song_elements.py

--- home_page_add_new_song_elements.py
+++ home_page_add_new_song_elements.py
@@ -13,3 +13,12 @@ genre_field = ft.TextField(value="", label="Жанр",label_style=ft.TextStyle(c
 album_field=ft.TextField(value="", label="Альбом",label_style=ft.TextStyle(color='#0ba6bf',size=22), width=500,border_color='#00718f',
                         cursor_color='#00718f',focused_border_color='#0ba6bf',border_radius=10,text_style=ft.TextStyle(color='#e8eee7',size=29),
                              border_width=2,selection_color='#81bece',cursor_width=4)
+
+def click_on_cong(e):
+    print(e.control.title.value)
+    if e.control.title.value=='Anime5':
+        print('hello')
+
+all_songs_column=ft.Column(controls=[],scroll=ft.ScrollMode.ALWAYS,height=700)
+
+
